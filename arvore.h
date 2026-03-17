9e222cd107c3436312a56d37caa6f1a5d871a3ab
Ecomic25 <arthurcesarabcd@gmail.com>
2026-03-17 13:28:31 -0300

    Add successor and predecessor search functions

--- arvore.h
+++ arvore.h
@@ -14,11 +14,13 @@ no* construir(int array[], int inicio, int fim);
 no* inserir(no* raiz, int v);
 no* buscarMenor(no* raiz);
 no* buscarMaior(no* raiz);
+no* buscarSucessor(no* raiz, int v);
+no* buscarPredecessor(no* raiz, int v);
 no* buscarValor(no* raiz, int v);
 no* remover(no* raiz, int v);
 int tamanho(no* raiz);
 int folhas(no* raiz);
 void destruir(no* raiz);
-void imprimir(no* raiz, int nivel);
+void imprimir(no* raiz, int n);
 
 #endif
