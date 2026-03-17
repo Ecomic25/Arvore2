11813ebe8acbb74701e82d210d9fb06a56a0247c
Ecomic25 <arthurcesarabcd@gmail.com>
2026-03-17 13:28:13 -0300

    Refactor print statements and improve formatting

--- main.c
+++ main.c
@@ -3,62 +3,76 @@
 #include "arvore.h"
 
 int main()
-{   
+{
     no* raiz = NULL;
     
     int array[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
     int tamanho_array = sizeof(array) / sizeof(array[0]);
     
-    printf("----CONSTRUINDO ARVORE FODAPLAYS----\n\n");
+    printf("\n------Construindo arvore muito foda------\n\n");
     
     raiz = construir(array, 0, tamanho_array - 1);
     
     imprimir(raiz, 0);
     
-    printf("\n----------------------------------------------\n");
-
+    printf("\n----------------------------------------------------------\n");
     printf("\nTotal de nos: %d", total_nos);
-    printf("\nTotal nos(recursão): %d", tamanho(raiz));
-    printf("\nTotal folhas: %d", folhas(raiz));
-    
-    printf("\n----------------------------------------------\n");
+    printf("\nTotal de nos(recursão): %d", tamanho(raiz));
+    printf("\nTotal de folhas: %d", folhas(raiz));
+    printf("\n\n----------------------------------------------------------\n");
     
     no* menor = buscarMenor(raiz);
     no* maior = buscarMaior(raiz);
     no* alvo = buscarValor(raiz, 20);
+    no* sucessor = buscarSucessor(raiz, 20);
+    no* predecessor = buscarPredecessor(raiz, 20);
     
     if(menor == NULL){
         printf("\nFunção de buscar menor falhou!");
     }
     else{
-        printf("\nO menor é %d", menor -> valor);
+        printf("\nO menor valor é %d", menor -> valor);
     }
     if(maior == NULL){
-        printf("\nFunção de buscar maior falhou!");
+        printf("\nFunção de buscar maior falhou");
     }
     else{
-        printf("\nO maior é %d", maior -> valor);
-    }
-    if(alvo == NULL){
-        printf("\nValor alvo não foi encontrado!");
-    }
-    else{
-        printf("\nNumero alvo: %d", alvo -> valor);
+        printf("\nO maior valor é %d", maior -> valor);
     }
     
-    printf("\n----------------------------------------------\n");
+    printf("\n\nBuscando informações do numero 20\n");
+    
+    if(alvo == NULL){
+        printf("\nNumero alvo nao foi encontrado");
+    }
+    else{
+        printf("\nO numero %d foi encontrado na arvore", alvo -> valor);
+    }
+    if(sucessor == NULL){
+        printf("\nFunção buscar sucessor falhou");
+    }
+    else{
+        printf("\nO sucessor é %d", sucessor -> valor);
+    }
+    if(predecessor == NULL){
+        printf("\nFunção de buscar predecessor falhou");
+    }
+    else{
+        printf("\nO predecessor é %d", predecessor -> valor);
+    }
+    
+    printf("\n\n----------------------------------------------------------\n");
     
     remover(raiz, 50);
-    printf("\n");
-    imprimir(raiz, 0);
-    printf("\nTotal de nos: %d", total_nos);
+    printf("\nRemovendo valor 50 da arvore!\n\n");
     
-    printf("\n----------------------------------------------\n");
+    imprimir(raiz, 0);
+    printf("\n\nTotal de nos: %d", total_nos);
     
     destruir(raiz);
-    raiz = NULL;
+    printf("\n\n--------Destruindo a arvore--------\n");
     printf("\nTotal de nos: %d", total_nos);
     
-    printf("\n\n----ENCERRANDO ARVORE FODAPLAYS----\n\n");
+    printf("\n\n----------Encerrando arvore----------");
     return 0;
 }
